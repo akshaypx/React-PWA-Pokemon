a153a61e798044b565150021382a26dfb6318fc4
NAGARRO\akshaysrivastava <akshay.srivastava@nagarro.com>
2022-10-03 16:41:19 +0530

    initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import { getAllPokemonList } from "./api/pokemon";
 function App() {
+  const [PokeData, setPokeData] = useState([]);
+
+  async function fetchAll() {
+    const data = await getAllPokemonList();
+    setPokeData(data?.results);
+  }
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center m-5">PokeMon List</h1>
+      <div className="container mt-5">
+        <div className="row">
+          {PokeData?.map((poke, i) => {
+            return (
+              <div
+                key={i}
+                // style={{
+                //   border: "2px solid grey",
+                //   borderRadius: "25px",
+                //   backgroundColor: "grey",
+                // }}
+                className="col-sm-6 col-md-4 col-lg-3 text-center p-2"
+              >
+                <img
+                  src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}
+                  alt=""
+                  style={{ width: "200px", height: "200px" }}
+                />
+                <p>{poke.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
